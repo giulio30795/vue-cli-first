98d45989ff908208af382027398598639fe8eeb5
giulio30795 <giulioincarbone95@gmail.com>
2021-11-22 14:20:49 +0100

    title

---
+++ src/components/title.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>La mia prima App con Vue CLI!</h1>
+    </header>
+</template>
+
+<script>
+export default {
+name: 'title' ,
+}
+</script>
+
+<style>
+
+</style>
